c18a2b463a9e7feb0a1c934fba20ed2db5905847
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-04-05 13:13:52 +0300

    done'

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+server.use(express.static('./public'));
+
+server.get('/', (request, response) => {
+    response.send('You are doing great');
+});
+
+server.use('*', (request,response) =>{
+    response.send('sorry this page not fond');
+})
+
+
+server.listen(PORT, () => {
+    console.log('hello there that is my port ', PORT);
+});
